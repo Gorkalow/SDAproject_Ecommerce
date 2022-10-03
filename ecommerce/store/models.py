25b883f14e7764ac867ef2bc5c28267ebad00d77
grzegorzgrzegorzak <grzegorz.grzegorzak94@gmail.com>
2022-10-03 22:02:35 +0200

    Render Products

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import OneToOneField, CharField, CASCADE, Model, \
-    FloatField, BooleanField, ForeignKey, SET_NULL, DateField
+    FloatField, BooleanField, ForeignKey, SET_NULL, DateField, IntegerField
 
 
 # Create your models here.
@@ -10,6 +10,7 @@ class Customer(Model):
     user = OneToOneField(User, null=True, blank=True, on_delete=CASCADE)
     name = CharField(max_length=200, null=True)
     email = CharField(max_length=200)
+
 
 class Product(Model):
     name = CharField(max_length=200)
@@ -21,10 +22,31 @@ class Product(Model):
 
 
 class Order(Model):
-    customer = ForeignKey(Customer, on_delete=SET_NULL, blank=True, null=True) # po co nam zamówienie gdy nie ma klienta?
+    customer = ForeignKey(Customer, on_delete=SET_NULL, blank=True,
+                          null=True)  # po co nam zamówienie gdy nie ma klienta?
     date_order = DateField(auto_now_add=True)
     complete = BooleanField(default=False, null=True, blank=False)
     transaction_id = CharField(max_length=200, null=True)
 
     def __str__(self):
         return str(self.id)
+
+
+class OrderItem(Model):
+    product = ForeignKey(Product, on_delete=SET_NULL, blank=True, null=True)
+    order = ForeignKey(Order, on_delete=SET_NULL, blank=True, null=True)
+    quantity = IntegerField(default=0, null=True, blank=True)
+    date_added = DateField(auto_now_add=True)
+
+
+class ShippingAddress(Model):
+    customer = ForeignKey(Customer, on_delete=SET_NULL, null=True)
+    order = ForeignKey(Order, on_delete=SET_NULL, null=True)
+    address = CharField(max_length=200, null=False)
+    city = CharField(max_length=200, null=True)
+    state = CharField(max_length=200, null=True)
+    zipcode = CharField(max_length=200, null=True)
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
