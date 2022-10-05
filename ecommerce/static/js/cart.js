fa588027d8f0d60345a1aea23189d4a1b1a46cfe
Aleksander Gorkalow <gorkahigh@gmail.com>
2022-10-06 00:38:20 +0200

    -added js script to main html to check if user is logged in. If not - the value will be 'AnonymousUser'. This user variable could be accessed within cart.js, because the cart file is at the bottom of main.html
    -added logic to user variable in cart.js

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,5 +7,12 @@ for (var i = 0; i < updateButtons.length; i++) {
         var productId = this.dataset.product
         var action = this.dataset.action
         console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:', user)
+        if(user == 'AnonymousUser'){
+            console.log('Not logged in')
+        }else{
+            console.log('User is logged in, sending data')
+        }
     })
 }
