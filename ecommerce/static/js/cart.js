7c3c40cbca75d78ed35da1e2885d5aba673db9f5
Aleksander Gorkalow <gorkahigh@gmail.com>
2022-10-06 00:14:34 +0200

    Adding to cart: -added class update-cart to the button in store.html; 'product' and 'action' custom attributes added to the button; - started making event handler in cart.js

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -1,2 +1,11 @@
-console.log('Hello world')
-
+//creating event handler for each button
+//1)querrying all of the cart items by the classname
+var updateButtons = document.getElementsByClassName('update-cart')
+// looping through all buttons
+for (var i = 0; i < updateButtons.length; i++) {
+    updateButtons[i].addEventListener('click', function (){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+    })
+}
