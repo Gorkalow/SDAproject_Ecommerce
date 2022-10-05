f5484d4b5a6ad790f1d40a0267a64fa3aafad634
Aleksander Gorkalow <gorkahigh@gmail.com>
2022-10-05 02:58:40 +0200

    Cart's totals modified. -created function for OrderItem and then for Order to evaluate the total prices and quantities of products. -view and cart were modified accordingly.

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -46,12 +46,28 @@ class Order(Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 class OrderItem(Model):
     product = ForeignKey(Product, on_delete=SET_NULL, blank=True, null=True)
     order = ForeignKey(Order, on_delete=SET_NULL, blank=True, null=True)
     quantity = IntegerField(default=0, null=True, blank=True)
     date_added = DateField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(Model):
