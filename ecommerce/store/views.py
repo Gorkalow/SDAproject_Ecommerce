27447e2aef7afa7cbc9d5f6957963c3a6efa43bc
Aleksander Gorkalow <gorkahigh@gmail.com>
2022-10-06 04:01:08 +0200

    Added logic to function updateItem so the quantity of the objects is increasing in cart, while adding a new objects and would be decreasing while deleting.

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import *
+import json
+
 
 # Create your views here.
 
@@ -11,33 +13,54 @@ def store(request):
 
 
 def cart(request):
-
     if request.user.is_authenticated:
         customer = request.user.customer
-        #get_or_create function first tries to query an object with certain values
+        # get_or_create function first tries to query an object with certain values
         # if that object doesn't exist, it creates it. We want an order with opened cart, thus setting complete to False
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        #we're able to query child objects like 'orderitem_set' by setting a value of parent objects like 'order'
+        # we're able to query child objects like 'orderitem_set' by setting a value of parent objects like 'order'
         items = order.orderitem_set.all()
     else:
         items = []
-        #creating values for unauthenticated user to prevent error while accessing empty cart
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        # creating values for unauthenticated user to prevent error while accessing empty cart
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
 
-    context = {'items':items, 'order':order}
+    context = {'items': items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
     if request.user.is_authenticated:
-            customer = request.user.customer
-            order, created = Order.objects.get_or_create(customer=customer, complete=False)
-            items = order.orderitem_set.all()
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
-    context = {'items':items, 'order':order}
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
 
+
 def updateItem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+
+    print('Action:', action)
+    print('ProductId:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity +1)
+    elif action == 'remove':
+        orderItem.quantity = (orderItem.quantity -1)
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
